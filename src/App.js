9ff0d8726324b2fd47a04fcd509e1f945150f6f7
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-05 23:13:28 +0900

    003

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImportCompoenet from './R003_ImportComponenet'
 
 function App() {
   return (
     <div>
       <h1>Start react 200</h1>
       <p> CSS 적용하기</p>
+      <ImportCompoenet/>
     </div>
     );
 }
