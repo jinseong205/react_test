a86f7db5d9268e3f731d4a2fc4dca38731fa4dc4
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-16 01:58:50 +0900

    022

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ImportCompoenet from './R018_PropsDataType';
+import ImportCompoenet from './R022_PropsDefault';
 
 function App() {
   return (
@@ -8,13 +8,8 @@ function App() {
       <h1>Start react 200</h1>
       <p> CSS 적용하기</p>
       <ImportCompoenet 
-        String = "react"
-        Number = {200}
-        Boolean = {1==1}
-        Array = {[0,1,8]}
-        ObjectJson = {{react:"리액트", twohundread:"200"}}
-        Function={console.log("FunctionProps : function!")}
-        />
+         ReactNumber={200}
+       />
     </div>
     );
 }
