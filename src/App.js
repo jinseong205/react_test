467c6343ab13e43e5ec08eb14637fe686fb4b0c4
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-16 23:29:59 +0900

    026

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import ImportCompoenet from './R025_SetSate';
+import ImportCompoenet from './R026_ForceUpdate';
 
 function App() {
   return (
@@ -8,7 +8,6 @@ function App() {
       <h1>Start react 200</h1>
       <p> CSS 적용하기</p>
       <ImportCompoenet> 
-        <span>node from app.js</span>
       </ImportCompoenet>
     </div>
     );
---
+++ src/R026_ForceUpdate.js
@@ -0,0 +1,32 @@
+
+import React, {Component} from 'react';
+
+class R026_ForceUpdate extends Component{
+
+    constructor(props){
+        super();
+        this.state = {
+            StateString : 'react',
+        }
+    }
+
+    StateChange = () =>{
+        this.state.StateString = '리액트';
+        this.forceUpdate();
+    }
+
+    render(){
+        return(
+            <div style = {{padding : '0px'}}>
+                <button onClick = {(e) => this.StateChange('direct', e)}>
+                    state 직접변경
+                </button><br/>
+                [state 변경하기] stateString : {this.state.StateString}
+            </div>
+
+        )
+    }
+
+}
+
+export default R026_ForceUpdate;
