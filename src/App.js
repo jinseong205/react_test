899db87cd2e5988e478251e7d35e2c5581fc347e
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-19 18:24:37 +0900

    032

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import ImportCompoenet from './R030_FunctionComponent';
+import ImportCompoenet from './R032_Fragments';
 
 function App() {
   return (
     <div>
       <h1>Start react 200</h1>
       <p> CSS 적용하기</p>
-      <ImportCompoenet contents = "[This is Function Component]"> 
+      <ImportCompoenet> 
       </ImportCompoenet>
     </div>
     );
-}
+} 
 
 
 export default App;
---
+++ src/R031_ReactHook.js
@@ -0,0 +1,18 @@
+import react, {useState, useEffect} from 'react';
+
+function R031_ReactHook(props){
+    const [contents, setContents] = useState('[This is React]');
+
+    useEffect(() => {
+        console.log('useEffect');
+    });
+
+    return(
+        <div style = {{padding : "0px"}}>
+            <h2> {contents} </h2>
+            <button onClick ={()=> setContents('[This is hook]')}> 버튼 </button>
+        </div>
+    )
+}
+
+export default R031_ReactHook;
