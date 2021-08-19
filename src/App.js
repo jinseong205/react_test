fd5ca18312bdcbfeb420463ce7ac9dc4d8bbb561
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-19 18:04:53 +0900

    030

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ImportCompoenet from './R026_ForceUpdate';
+import ImportCompoenet from './R030_FunctionComponent';
 
 function App() {
   return (
     <div>
       <h1>Start react 200</h1>
       <p> CSS 적용하기</p>
-      <ImportCompoenet> 
+      <ImportCompoenet contents = "[This is Function Component]"> 
       </ImportCompoenet>
     </div>
     );
---
+++ src/R030_FunctionComponent.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+function R030_FunctionComponent(props){
+    let {contents} = props;
+
+    return(
+        <h2>{contents}</h2>
+    )
+}
+
+export default R030_FunctionComponent;
