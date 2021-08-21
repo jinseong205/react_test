91ac5eb4787802d8de742f6056e8ba5808d44600
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-08-21 20:04:35 +0900

    test

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send({'message':'node get success'});
+});
+
 router.post('/', function(req, res, next) {
   res.send({'message':'node post success'});
 });
