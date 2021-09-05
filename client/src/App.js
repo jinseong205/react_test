cdf094677e86851fca9e7a82a9b5909399964f3b
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-09-05 22:00:53 +0900

    react-redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
-import './App.css';
-import ImportCompoenet from './StrAddButton';
+import React, {Component} from 'react'
+import './App.css'
+import {connect} from 'react-redux'
+import ImportCompoenet from './StrAddButton'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
@@ -9,12 +10,23 @@ class App extends Component {
       return (
           <div>
               <h1>Start React 200!</h1>
-              <span>{this.props.store.getState().data.str}</span><br/>
-              <ImportCompoenet store={this.props.store}/>
+              {/* <span>{this.props.store.getState().data.str}</span><br/> */}
+              <span>{this.props.str}</span><br/>
+              {/* <ImportCompoenet store={this.props.store}/> */}
+              <ImportCompoenet AppProp="200"/>
           </div>
       );
   }
 }
+
+let mapStateToProps = (state, props) =>{
+    console.log('Props from index.js : ' + props.indexProp)
+    return {
+        str : state.data.str,
+    }
+}
+
+App = connect(mapStateToProps, null)(App);
 
 
 export default App;
