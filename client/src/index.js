554afcb1fc23e6ac9860ada08eddaf32e6eec61a
jinseong205 <jinseong205@gmail.com>
2021-09-06 21:47:03 +0900

    084

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const CallMiddleware = store => nextMiddle => action => {
+
+  console.log('1. reducer 실행전');
+  console.log('2. action type : ' + action.type  + ', stroe str : ' + store.getState().data.str);
+  
+  let result = nextMiddle(action);
+
+  console.log('3. reducer 실행전');
+  console.log('4. action type : ' + action.type  + ', stroe str : ' + store.getState().data.str);
+  
+  return result;
+}
+
+const store = createStore(reducers, applyMiddleware(CallMiddleware));
 
 const listener =() =>{
   ReactDOM.render(
