3693caa27defe45079bf4ee1371b4cba080fcb22
jinseong205 <jinseong205@gmail.com>
2021-09-14 22:33:49 +0900

    0914

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import './App.css'
-import {connect} from 'react-redux'
-import ImportCompoenet from './StrAddButton'
+import ImportCompoenet from './component/R085_cookieSave'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
@@ -10,23 +9,13 @@ class App extends Component {
       return (
           <div>
               <h1>Start React 200!</h1>
-              {/* <span>{this.props.store.getState().data.str}</span><br/> */}
-              <span>{this.props.str}</span><br/>
-              {/* <ImportCompoenet store={this.props.store}/> */}
               <ImportCompoenet AppProp="200"/>
           </div>
       );
   }
 }
 
-let mapStateToProps = (state, props) =>{
-    console.log('Props from index.js : ' + props.indexProp)
-    return {
-        str : state.data.str,
-    }
-}
 
-App = connect(mapStateToProps, null)(App);
 
 
 export default App;
