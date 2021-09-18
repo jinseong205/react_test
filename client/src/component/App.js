257f04a0f1fa9da214214cd28f836127c197af67
jinseong <57009926+jinseong205@users.noreply.github.com>
2021-09-18 19:35:01 +0900

    095

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from 'react';
 import { Route } from "react-router-dom";
-import reactRouter from './R089_reactRouter'
-import reactRouter2 from './R089_reactRouter2'
+import InnerComponent from './R095_reactThrottle';
 
 // css
 import '../css/new.css';
 
 // header
 import HeaderAdmin from './Header/Header admin';
-
 // footer
 import Footer from './Footer/Footer';
+//login
+import LoginForm from './LoginForm';
+import R094_reactDebounce from './R094_reactDebounce';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
         <HeaderAdmin/> 
-        <Route exact path='/' component={reactRouter} />
-        <Route exact path='/reactRouter2' component={reactRouter2} />
+        <Route exact path = '/' component={LoginForm} />
+        <Route exact path = '/throttle' component={InnerComponent} />
         <Footer/>
       </div>
     );
